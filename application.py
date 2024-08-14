1096ea10b1dfd9dc9390c0b0528141b60cc4989a
Ekaterina Varfolomeeva <varfolomeeva_97@bk.ru>
2024-08-14 12:55:31 +0300

    задала явно переменную для приложения для избежания проблем при импорте

--- application.py
+++ application.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3.7
 #-*- coding: UTF-8 -*-
-from bottle import route, run, template, request, redirect
+import bottle
+from bottle import template, request, redirect
 
-@route('/main', method=['GET', 'POST'])
+app = bottle.Bottle()
+@app.route('/main', method=['GET', 'POST'])
 def main_page():
     if request.method == 'POST':
         redirect('/address')
     return template('templates/main.html')
 
-@route('/address', method=['GET', 'POST'])
+@app.route('/address', method=['GET', 'POST'])
 def get_address():
     if request.method == 'POST':
         redirect('/main')
     return template('templates/address.html')
 
 if __name__ == '__main__':
-    run(host='localhost', port=8080, debug=True)
+    app.run(host='localhost', port=8080, debug=True)
