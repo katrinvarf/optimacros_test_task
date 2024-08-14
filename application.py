d8693f62decb594d8cee22942ba1d3ec849d1993
Ekaterina Varfolomeeva <varfolomeeva_97@bk.ru>
2024-08-14 18:25:08 +0300

    добавила получение токена из аргументов командной строки

--- application.py
+++ application.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3.7
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 import bottle
 from bottle import template, request, redirect
+import argparse
 
 app = bottle.Bottle()
+token = ''
+
+
 @app.route('/main', method=['GET', 'POST'])
 def main_page():
     if request.method == 'POST':
         redirect('/address')
     return template('templates/main.html')
+
 
 @app.route('/address', method=['GET', 'POST'])
 def get_address():
@@ -16,5 +21,12 @@ def get_address():
         redirect('/main')
     return template('templates/address.html')
 
+
 if __name__ == '__main__':
+    # получаем токен для DaData из аргументов командной строки
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--token', action='store', dest='token', type=str)
+    args = parser.parse_args()
+    token = args.token
+
     app.run(host='localhost', port=8080, debug=True)
