bcedd8292605a8a71927b687c4c97e4a8fa2f998
Ekaterina Varfolomeeva <varfolomeeva_97@bk.ru>
2024-08-15 15:51:37 +0300

    написала комментарии

--- getter.py
+++ getter.py
@@ -9,10 +9,12 @@ def get_address(token, latitude, longitude):
     try:
         dadata = Dadata(token)
         result = dadata.geolocate(name="address", lat=latitude, lon=longitude)
-        if len(result) != 0:
+
+        if len(result) != 0:  # проверка, что найден хотя бы один адрес
             full_address = result[0]['unrestricted_value']
             address_components = full_address.split(', ')
-    except httpx.ConnectError as e:
+
+    except httpx.ConnectError as e:  # обработка проблемы с соединением с API DaData
         print(e)
         address_components = None
 
