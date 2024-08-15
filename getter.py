76f84037c50cca77ef3d1ec4d5ef2472b6de2fd8
Ekaterina Varfolomeeva <varfolomeeva_97@bk.ru>
2024-08-15 15:03:04 +0300

    добавила обработку ошибок в случае проблем с соединением с API

--- getter.py
+++ getter.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3.7
 # -*- coding: UTF-8 -*-
 from dadata import Dadata
+import httpx
 
 
 def get_address(token, latitude, longitude):
-    dadata = Dadata(token)
-    result = dadata.geolocate(name="address", lat=latitude, lon=longitude)
     address_components = []
-    if len(result) != 0:
-        full_address = result[0]['unrestricted_value']
-        address_components = full_address.split(', ')
-    print(address_components)
+    try:
+        dadata = Dadata(token)
+        result = dadata.geolocate(name="address", lat=latitude, lon=longitude)
+        if len(result) != 0:
+            full_address = result[0]['unrestricted_value']
+            address_components = full_address.split(', ')
+    except httpx.ConnectError as e:
+        print(e)
+        address_components = None
+
     return address_components
